eb03664df6311907ae3eaf7e48926a1276408dfb
Víctor Matía Rodríguez <vmatir@gmail.com>
2015-10-02 23:16:38 +0200

    Added comments and updated variables initialization.
    Added comments to the code so that it can be understood easier and
    changed the way of initializing some variables, using the set functions
    instead of simply changing the value of the variable.

--- src/MatVGLSDL2Engine.cpp
+++ src/MatVGLSDL2Engine.cpp
@@ -17,25 +17,29 @@
 #include "MatVGLSDL2Engine.hpp"
 
 MatVGL::Engine *MatVGL::createGameEngine() {
+
+  // We create the pointer to the engine in a secure way.
   static MatVGL::SDL2Engine *s_gameEngine = nullptr;
   if (!s_gameEngine) {
     s_gameEngine = new MatVGL::SDL2Engine();
   }
 
+  // And we return it.
   return s_gameEngine;
 }
 
 MatVGL::SDL2Engine::SDL2Engine() {
-  p_bgColor.red = 255;
-  p_bgColor.green = 255;
-  p_bgColor.blue = 255;
-  p_bgColor.alpha = 255;
-
+  // Set the default values of the variables.
   p_windowProperties.title = "MatVGL Window";
   p_windowProperties.x = 0;
   p_windowProperties.y = 0;
   p_windowProperties.width = 0;
   p_windowProperties.height = 0;
+
+  p_bgColor.red = 255;
+  p_bgColor.green = 255;
+  p_bgColor.blue = 255;
+  p_bgColor.alpha = 255;
 
   p_viewportProperties.x = 0;
   p_viewportProperties.y = 0;
@@ -50,80 +54,109 @@ MatVGL::SDL2Engine::SDL2Engine() {
   this->_glContext = 0;
 }
 
+// Shut the engine down, if it hasn't been shut down, yet.
 MatVGL::SDL2Engine::~SDL2Engine() { this->shutDown(); }
 
 void MatVGL::SDL2Engine::startEngine() {
+  // Inform that we are starting up.
   p_isStartingUp = true;
 
+  // Initialize SDL Video module.
   SDL_Init(SDL_INIT_VIDEO);
 
-  p_windowProperties.x = SDL_WINDOWPOS_CENTERED;
-  p_windowProperties.y = SDL_WINDOWPOS_CENTERED;
+  // Set the window position.
+  this->setWindowPosition(SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
 
+  // Get the display size.
   SDL_DisplayMode current;
   SDL_GetCurrentDisplayMode(0, &current);
-  p_windowProperties.width = current.w;
-  p_windowProperties.height = current.h;
+  // And set the window's size to the display's size.
+  this->setWindowSize(current.w, current.h);
 
+  // Set the default SDL GL attributes.
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_PROFILE_CORE);
 
+  // Create the SDL window.
   this->_window = SDL_CreateWindow(
       p_windowProperties.title, p_windowProperties.x, p_windowProperties.y,
       p_windowProperties.width, p_windowProperties.height,
       SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN);
+  // Create the SDL context.
   this->_glContext = SDL_GL_CreateContext(this->_window);
 
+  // Initialize GLEW.
   GLenum glewError = glewInit();
   if (glewError != GLEW_OK) {
     abort(); // Until we error check properly.
   }
 
+  // Activate VSync (might want to move this into a settings portion).
   SDL_GL_SetSwapInterval(1);
 
+  // Clear the background of the window.
   glClearDepth(1.0f);
   glClearColor((float)p_bgColor.red / 255.0f, (float)p_bgColor.green / 255.0f,
                (float)p_bgColor.blue / 255.0f, (float)p_bgColor.alpha / 255.0f);
   glDepthRange(0.0f, 1.0f);
 
+  // Adjust the viewport to the window.
   this->adjustViewport();
 
+  // Inform that we have started up already.
   p_isStartingUp = false;
   p_isReadyForUse = true;
 }
 
 void MatVGL::SDL2Engine::startFrame() {
+  // Clear the buffers so that the newly rendered buffer can be put in the
+  // front.
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 }
 
-void MatVGL::SDL2Engine::endFrame() { SDL_GL_SwapWindow(this->_window); }
+void MatVGL::SDL2Engine::endFrame() {
+  // Swap the window's buffers.
+  SDL_GL_SwapWindow(this->_window);
+}
 
 void MatVGL::SDL2Engine::shutDown() {
+  // Inform that we are shutting the engine down.
   p_isShuttingDown = true;
   p_isReadyForUse = false;
 
+  // Destroy the GL context.
   if (_glContext) {
     SDL_GL_DeleteContext(this->_glContext);
   }
 
+  // Destroy the SDL window.
   if (_window) {
     SDL_DestroyWindow(this->_window);
   }
 
+  // Quit SDL2.
   SDL_Quit();
 
+  // We finished shutting down.
   p_isShuttingDown = false;
 }
 
 bool MatVGL::SDL2Engine::sleepForFrameLimit(UInt32 maxFrameRate) {
+  // This isn't finished, clearly.
+  // Returning false so that there's never an FPS cap apart from VSync.
   return false;
 }
 
+// All of the setters work in the same way: they change the variable or
+// variables if they are new, then updates the window if it's already open, when
+// necessary.
 void MatVGL::SDL2Engine::setWindowTitle(const char *title) {
+  // Set the window title if it's actually new.
   if (title != p_windowProperties.title) {
     p_windowProperties.title = title;
 
+    // Update the corresponding window property if the window is already open.
     if (this->_window) {
       SDL_SetWindowTitle(this->_window, p_windowProperties.title);
     }
@@ -171,9 +204,11 @@ void MatVGL::SDL2Engine::setBackgroundColor(Int16 red, Int16 green, Int16 blue,
 }
 
 void MatVGL::SDL2Engine::adjustViewport() {
+  // This function simply gets the window size.
   SDL_GetWindowSize(this->_window, (Int32 *)&p_windowProperties.width,
                     (Int32 *)&p_windowProperties.height);
 
+  // So as to then adjust the viewport to the whole screen.
   this->setViewportPosition(0, 0);
   this->setViewportSize(p_windowProperties.height, p_windowProperties.height);
 }
@@ -209,6 +244,7 @@ void MatVGL::SDL2Engine::setViewportSize(UInt32 width, UInt32 height) {
   }
 }
 
+// The getters simply return a variable.
 bool MatVGL::SDL2Engine::isStartingUp() { return p_isStartingUp; }
 
 bool MatVGL::SDL2Engine::isReadyForUse() { return p_isReadyForUse; }
@@ -216,6 +252,8 @@ bool MatVGL::SDL2Engine::isReadyForUse() { return p_isReadyForUse; }
 bool MatVGL::SDL2Engine::isShuttingDown() { return p_isShuttingDown; }
 
 bool MatVGL::SDL2Engine::hasTheUserXedOut() {
+  // In this getter, though, we have to check for the SDL events, and get the
+  // QUIT event.
   while (SDL_PollEvent(&_event) != 0) {
     if (_event.type == SDL_QUIT) {
       return true;
